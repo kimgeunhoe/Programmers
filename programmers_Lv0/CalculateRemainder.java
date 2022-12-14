7dbbd738340fa7d2d2bf75a69c543266c284a1ad
kimgeunhoe <ghkim1031@naver.com>
2022-12-14 21:33:13 +0900

    Create CalculateRemainder.java

---
+++ programmers_Lv0/CalculateRemainder.java
@@ -0,0 +1,12 @@
+package programmers_Lv0;
+
+public class CalculateRemainder {
+
+	public static void main(String[] args) {
+		int num1 = 3;
+		int num2 = 2;
+
+		System.out.println(num1%num2);
+	}
+
+}
