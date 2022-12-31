798766fdd75fbe137cdc214f0cb1b4809c7c6964
kimgeunhoe <ghkim1031@naver.com>
2022-12-31 14:46:57 +0900

    Create MakeArrayDouble.java

---
+++ programmers_Lv0/MakeArrayDouble.java
@@ -0,0 +1,14 @@
+package programmers_Lv0;
+
+public class MakeArrayDouble {
+
+	public static void main(String[] args) {
+		int[] numbers = {1, 2, 3, 4, 5};
+
+	    for(int i=0; i<numbers.length; i++) {
+	    	numbers[i] *= 2;
+	    	System.out.println(numbers[i]);
+	    }
+	}
+
+}
