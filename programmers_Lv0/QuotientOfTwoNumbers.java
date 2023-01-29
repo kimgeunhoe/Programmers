fc3dd292cdbd4a7a896f9a5e0dad2f7907926109
kimgeunhoe <ghkim1031@naver.com>
2023-01-29 20:51:24 +0900

    Create QuotientOfTwoNumbers.java

---
+++ programmers_Lv0/QuotientOfTwoNumbers.java
@@ -0,0 +1,12 @@
+package programmers_Lv0;
+
+public class QuotientOfTwoNumbers {
+
+	public static void main(String[] args) {
+		int num1 = 10;
+		int num2 = 5;
+
+		System.out.println(num1 / num2);
+	}
+
+}
