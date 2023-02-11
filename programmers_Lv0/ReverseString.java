7e9e91fff56989a39ad296fa73bb58c27ea37be7
kimgeunhoe <ghkim1031@naver.com>
2023-02-11 19:55:57 +0900

    Create ReverseString.java

---
+++ programmers_Lv0/ReverseString.java
@@ -0,0 +1,12 @@
+package programmers_Lv0;
+
+public class ReverseString {
+
+	public static void main(String[] args) {
+		String my_string = "jaron";
+	    StringBuilder sb = new StringBuilder(my_string);
+	    
+	    System.out.println(sb.reverse().toString());
+	}
+
+}
