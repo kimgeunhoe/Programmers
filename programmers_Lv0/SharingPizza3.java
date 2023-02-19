068b10b29b7672b54ece0e3d6dfc31073ea701b1
kimgeunhoe <ghkim1031@naver.com>
2023-02-19 13:39:37 +0900

    Create SharingPizza3.java

---
+++ programmers_Lv0/SharingPizza3.java
@@ -0,0 +1,12 @@
+package programmers_Lv0;
+
+public class SharingPizza3 {
+
+	public static void main(String[] args) {
+		int slice = 7;
+		int n = 10;
+  
+	    System.out.println(n/slice + (n%slice!=0 ? 1 : 0));
+	}
+
+}
