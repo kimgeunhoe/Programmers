a5724ab7b4bf27186fae028b42ff4838e7479d8a
kimgeunhoe <ghkim1031@naver.com>
2023-02-25 19:33:31 +0900

    Create SumOfTwoNumbers.java

---
+++ programmers_Lv0/SumOfTwoNumbers.java
@@ -0,0 +1,12 @@
+package programmers_Lv0;
+
+public class SumOfTwoNumbers {
+
+	public static void main(String[] args) {
+		int num1 = 2;
+		int num2 = 3;
+
+		System.out.println(num1 + num2);
+	}
+
+}
