70139348b574a5a52ab9ea2e3026ba8bca0fd46e
kimgeunhoe <ghkim1031@naver.com>
2023-03-21 19:43:23 +0900

    Create ConvertStringToInt.java

---
+++ programmers_Lv1/ConvertStringToInt.java
@@ -0,0 +1,12 @@
+package programmers_Lv1;
+
+public class ConvertStringToInt {
+
+	public static void main(String[] args) {
+		String s = "-1234";
+	    int answer = Integer.parseInt(s);
+
+	    System.out.println(answer);
+	}
+
+}
