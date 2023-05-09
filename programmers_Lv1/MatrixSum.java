bc14de0229a66e1a9466ed17db9e9cb525f63f16
kimgeunhoe <ghkim1031@naver.com>
2023-05-09 22:21:07 +0900

    Create MatrixSum.java

---
+++ programmers_Lv1/MatrixSum.java
@@ -0,0 +1,23 @@
+package programmers_Lv1;
+
+public class MatrixSum {
+
+	public static void main(String[] args) {
+		int[][] arr1 = {{1, 2}, {2, 3}};
+		int[][] arr2 = {{3, 4}, {5, 6}};
+		int[][] answer = new int[arr1.length][arr1[0].length];
+		
+		for(int i=0; i<arr1.length; i++) {
+			for(int j=0; j<arr1[0].length; j++) {
+				answer[i][j] = arr1[i][j] + arr2[i][j];
+			}
+		}
+		
+		for(int[] ans : answer) {
+			for(int a : ans) {
+				System.out.println(a);
+			}
+		}
+	}
+
+}
