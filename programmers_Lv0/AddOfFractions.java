4cb059ca0ce7f44bc972616d3d503875494a6263
kimgeunhoe <ghkim1031@naver.com>
2023-06-18 17:46:39 +0900

    Update AddOfFractions.java

--- programmers_Lv0/AddOfFractions.java
+++ programmers_Lv0/AddOfFractions.java
@@ -1,34 +1,24 @@
 package programmers_Lv0;
 
-public class AddOfFractions {
+import java.util.Arrays;
 
+public class AddOfFractions {
+//	https://school.programmers.co.kr/learn/courses/30/lessons/120808
 	public static void main(String[] args) {
-		int denum1 = 1;
-		int num1 = 2;
-		int denum2 = 3;
-		int num2 = 4;
-		int[] answer = new int[2];
-		int min = Math.min(num1, num2);
-		
-		for(int i=min; i>0; i--) {
-	    	if(num1%i==0 && num2%i==0) {
-	    		answer[1] = num1 * num2 / i;
+		int numer1 = 1;
+		int denom1 = 2;
+		int numer2 = 3;
+		int denom2 = 4;
+		int[] answer = new int[] {numer1 * denom2 + numer2 * denom1, denom1*denom2};
+        
+        for(int min = Math.min(answer[0], answer[1]); min>1; min--) {
+	    	if(answer[0]%min==0 && answer[1]%min==0) {
+	    		answer[0] /= min;
+	    		answer[1] /= min;
 	    		break;
 	    	}
 	    }
 		
-		answer[0] = denum1 * answer[1] / num1 + denum2 * answer[1] / num2;
-		
-		min = Math.min(answer[0], answer[1]);
-		
-		for(int i=min; i>0; i--) {
-	    	if(answer[0]%i==0 && answer[1]%i==0) {
-	    		answer[0] /= i;
-	    		answer[1] /= i;
-	    		break;
-	    	}
-	    }
-		
-		System.out.println(answer[0] + " " + answer[1]);
+        System.out.println(Arrays.toString(answer));
 	}
 }
