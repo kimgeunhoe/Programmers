73b7f8ccdb39186d28080b505e6ebd1e71d74918
kimgeunhoe <ghkim1031@naver.com>
2023-06-19 22:40:25 +0900

    Update CountOddEven.java

--- programmers_Lv0/CountOddEven.java
+++ programmers_Lv0/CountOddEven.java
@@ -1,18 +1,20 @@
 package programmers_Lv0;
 
-public class CountOddEven {
+import java.util.Arrays;
 
+public class CountOddEven {
+//	https://school.programmers.co.kr/learn/courses/30/lessons/120824
 	public static void main(String[] args) {
 		int[] num_list = {1, 2, 3, 4, 5};
         int[] answer = {0, 0};
 
         for(int n : num_list) {
-        	answer[n%2==0 ? 0 : 1] += 1;
+        	answer[n%2==0 ? 0 : 1]++;
 //        	if(n!=0) {
 //        	} //0도 짝수취급?
         }
         
-        System.out.println(answer[0] + " " + answer[1]);
+        System.out.println(Arrays.toString(answer));
 	}
 
 }
