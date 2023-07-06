03b00dc765bf7743755bb307cfd27d8dc2bce623
kimgeunhoe <ghkim1031@naver.com>
2023-07-06 21:06:47 +0900

    Create BacterialGrowth.java

---
+++ programmers_Lv0/BacterialGrowth.java
@@ -0,0 +1,13 @@
+package programmers_Lv0;
+
+public class BacterialGrowth {
+
+	public static void main(String[] args) {
+		int n = 7;
+		int t = 15;
+
+        System.out.println(n * (int)Math.pow(2, t));
+        System.out.println(n << t);
+	}
+
+}
