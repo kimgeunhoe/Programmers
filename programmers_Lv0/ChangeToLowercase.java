c9e9b601a1adf59f65e6ab1404b1e60e957c6bb5
kimgeunhoe <ghkim1031@naver.com>
2023-07-13 20:10:14 +0900

    Create ChangeToLowercase.java

---
+++ programmers_Lv0/ChangeToLowercase.java
@@ -0,0 +1,11 @@
+package programmers_Lv0;
+
+public class ChangeToLowercase {
+//	https://school.programmers.co.kr/learn/courses/30/lessons/181876
+	public static void main(String[] args) {
+		String myString = "aBcDeFg";
+		
+		System.out.println(myString.toLowerCase());
+	}
+
+}
