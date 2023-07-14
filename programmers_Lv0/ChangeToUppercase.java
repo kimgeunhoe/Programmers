e6a41c9a63ef2626e6ca6cf268913ab3007bc255
kimgeunhoe <ghkim1031@naver.com>
2023-07-14 22:20:44 +0900

    Create ChangeToUppercase.java

---
+++ programmers_Lv0/ChangeToUppercase.java
@@ -0,0 +1,11 @@
+package programmers_Lv0;
+
+public class ChangeToUppercase {
+//	https://school.programmers.co.kr/learn/courses/30/lessons/181877
+	public static void main(String[] args) {
+		String myString = "aBcDeFg"	;
+		
+		System.out.println(myString.toUpperCase());
+	}
+
+}
