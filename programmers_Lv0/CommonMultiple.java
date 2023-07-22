dd0a1d091e2388d0af6bb570d66ce80630e85299
kimgeunhoe <ghkim1031@naver.com>
2023-07-22 13:01:05 +0900

    Create CommonMultiple.java

---
+++ programmers_Lv0/CommonMultiple.java
@@ -0,0 +1,13 @@
+package programmers_Lv0;
+
+public class CommonMultiple {
+//	https://school.programmers.co.kr/learn/courses/30/lessons/181936
+	public static void main(String[] args) {
+		int number = 60;
+		int n = 2;
+		int m = 3;
+		
+		System.out.println(number%n==0 && number%m==0 ? 1 : 0);
+	}
+
+}
