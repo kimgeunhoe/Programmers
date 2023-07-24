36ff217138141f56830557b83d0ce5e9f9f6e83d
kimgeunhoe <ghkim1031@naver.com>
2023-07-24 21:23:28 +0900

    Create CompareDates.java

---
+++ programmers_Lv0/CompareDates.java
@@ -0,0 +1,14 @@
+package programmers_Lv0;
+
+import java.util.Arrays;
+
+public class CompareDates {
+
+	public static void main(String[] args) {
+		int[] date1 = {2021, 12, 28};
+		int[] date2 = {2021, 12, 29};
+
+		//System.out.println(Arrays.compare(date1, date2)<0 ? 1 : 0);//java 1.9 이상
+	}
+
+}
