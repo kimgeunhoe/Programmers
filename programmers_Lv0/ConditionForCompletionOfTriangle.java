855d36d038f946300ae0f3942f0a1b45c0144d4e
kimgeunhoe <ghkim1031@naver.com>
2023-07-25 21:21:21 +0900

    Create ConditionForCompletionOfTriangle.java

---
+++ programmers_Lv0/ConditionForCompletionOfTriangle.java
@@ -0,0 +1,11 @@
+package programmers_Lv0;
+
+public class ConditionForCompletionOfTriangle {
+
+	public static void main(String[] args) {
+		int[] sides = {1, 2};
+
+		System.out.println(2 * Math.min(sides[0], sides[1]) - 1);
+	}
+
+}
