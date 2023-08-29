4a22f4e7f7c3223a794be3433302896dcf25807e
kimgeunhoe <ghkim1031@naver.com>
2023-08-29 20:59:53 +0900

    Create DeterminingTheNumberOfSquares.java

---
+++ programmers_Lv0/DeterminingTheNumberOfSquares.java
@@ -0,0 +1,11 @@
+package programmers_Lv0;
+
+public class DeterminingTheNumberOfSquares {
+
+	public static void main(String[] args) {
+		int n = 144;
+		
+		System.out.println(Math.pow((int)Math.sqrt(n), 2)==n ? 1 : 2);
+	}
+
+}
