ae2ede8b3e7c7fa5d976e90f4395792531741798
kimgeunhoe <ghkim1031@naver.com>
2023-09-07 22:34:42 +0900

    Create EmphasizeA.java

---
+++ programmers_Lv0/EmphasizeA.java
@@ -0,0 +1,11 @@
+package programmers_Lv0;
+
+public class EmphasizeA {
+//	https://school.programmers.co.kr/learn/courses/30/lessons/181874
+	public static void main(String[] args) {
+		String myString = "abstract algebra";
+		
+		System.out.println(myString.toLowerCase().replace("a", "A"));
+	}
+
+}
