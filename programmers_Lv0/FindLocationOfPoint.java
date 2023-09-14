6a16e87fae5c9030d74cffcfb75e0b36e7ad410e
kimgeunhoe <ghkim1031@naver.com>
2023-09-14 21:30:14 +0900

    Create FindLocationOfPoint.java

---
+++ programmers_Lv0/FindLocationOfPoint.java
@@ -0,0 +1,12 @@
+package programmers_Lv0;
+
+public class FindLocationOfPoint {
+
+	public static void main(String[] args) {
+		int[] dot = {2, 4};
+        int answer = dot[0]*dot[1]>0 ? 1 : 2;
+        
+        System.out.println(answer += dot[1]<0 ? 2 : 0);
+	}
+
+}
