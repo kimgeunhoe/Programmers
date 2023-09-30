8ba91042a1a1489a858a89333eaf5a4965f275b5
kimgeunhoe <ghkim1031@naver.com>
2023-09-30 12:21:17 +0900

    Create LambSkewer.java

---
+++ programmers_Lv0/LambSkewer.java
@@ -0,0 +1,12 @@
+package programmers_Lv0;
+
+public class LambSkewer {
+
+	public static void main(String[] args) {
+		int n = 10;
+		int k = 3;
+
+        System.out.println(n * 12000 + (k - n/10) * 2000);
+	}
+
+}
