22f0c103c4139432846a667593d070fe3f9caf67
kimgeunhoe <ghkim1031@naver.com>
2023-10-06 22:34:28 +0900

    Create Letter.java

---
+++ programmers_Lv0/Letter.java
@@ -0,0 +1,11 @@
+package programmers_Lv0;
+
+public class Letter {
+
+	public static void main(String[] args) {
+		String message = "happy birthday!";
+
+		System.out.println(message.length() * 2);
+	}
+
+}
