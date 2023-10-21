452d6d51cee9f0f095cf8bfc6cb64f7c105089cf
kimgeunhoe <ghkim1031@naver.com>
2023-10-21 19:52:27 +0900

    Create MultipleOfN.java

---
+++ programmers_Lv0/MultipleOfN.java
@@ -0,0 +1,12 @@
+package programmers_Lv0;
+
+public class MultipleOfN {
+//	https://school.programmers.co.kr/learn/courses/30/lessons/181937
+	public static void main(String[] args) {
+		int num = 98;
+		int n = 2;
+
+		System.out.println(num%n==0 ? 1 : 0);
+	}
+
+}
