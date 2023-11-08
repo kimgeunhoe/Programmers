ec38a6bf390141d855edb539f5cb59c133e37330
kimgeunhoe <ghkim1031@naver.com>
2023-11-08 20:32:03 +0900

    Create OutputString.java

---
+++ programmers_Lv0/OutputString.java
@@ -0,0 +1,14 @@
+package programmers_Lv0;
+
+import java.util.Scanner;
+
+public class OutputString {
+//	https://school.programmers.co.kr/learn/courses/30/lessons/181952
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        
+        System.out.print(a);
+	}
+
+}
