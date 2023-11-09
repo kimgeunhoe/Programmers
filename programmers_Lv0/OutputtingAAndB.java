f59328dd8a3b38c7cd11c9764c69a9d9b7472fa9
kimgeunhoe <ghkim1031@naver.com>
2023-11-09 22:03:21 +0900

    Create OutputtingAAndB.java

---
+++ programmers_Lv0/OutputtingAAndB.java
@@ -0,0 +1,15 @@
+package programmers_Lv0;
+
+import java.util.Scanner;
+
+public class OutputtingAAndB {
+//	https://school.programmers.co.kr/learn/courses/30/lessons/181951
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.printf("a = %d \nb = %d", a, b);
+	}
+
+}
