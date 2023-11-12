67dd2d1cbc1ed0beb49904954df1a6570d92504b
kimgeunhoe <ghkim1031@naver.com>
2023-11-12 15:09:59 +0900

    Create PaperCutting.java

---
+++ programmers_Lv0/PaperCutting.java
@@ -0,0 +1,12 @@
+package programmers_Lv0;
+
+public class PaperCutting {
+
+	public static void main(String[] args) {
+		int M = 2;
+		int N = 2;
+		
+        System.out.println(M * N - 1);
+	}
+
+}
