92296684f3cb063f540a263a0af830971707ae79
kimgeunhoe <ghkim1031@naver.com>
2023-11-15 22:24:53 +0900

    Create PrintingSpecialCharacters.java

---
+++ programmers_Lv0/PrintingSpecialCharacters.java
@@ -0,0 +1,9 @@
+package programmers_Lv0;
+
+public class PrintingSpecialCharacters {
+//	https://school.programmers.co.kr/learn/courses/30/lessons/181948
+	public static void main(String[] args) {
+		System.out.print("!@#$%^&*(\\'\"<>?:;");
+	}
+
+}
