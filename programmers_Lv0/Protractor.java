715e22a372b4a00480c243d0c82f5db2b49ed236
kimgeunhoe <ghkim1031@naver.com>
2023-11-20 21:52:07 +0900

    Create Protractor.java

---
+++ programmers_Lv0/Protractor.java
@@ -0,0 +1,11 @@
+package programmers_Lv0;
+
+public class Protractor {
+
+	public static void main(String[] args) {
+		int angle = 70;
+
+		System.out.println(angle / 90 + (angle<=90 ? 1 : 2));
+	}
+
+}
