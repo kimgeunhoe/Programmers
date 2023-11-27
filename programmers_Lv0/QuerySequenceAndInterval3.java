5ccb471e80d8afa676ac8607082247f67d1b7970
kimgeunhoe <ghkim1031@naver.com>
2023-11-27 22:36:34 +0900

    Create QuerySequenceAndInterval3.java

---
+++ programmers_Lv0/QuerySequenceAndInterval3.java
@@ -0,0 +1,21 @@
+package programmers_Lv0;
+
+import java.util.Arrays;
+
+public class QuerySequenceAndInterval3 {
+//	https://school.programmers.co.kr/learn/courses/30/lessons/181924
+	public static void main(String[] args) {
+		int[] arr = {0, 1, 2, 3, 4};
+		int[][] queries = {{0, 3}, {1, 2}, {1, 4}};
+		int temp = 0;
+		
+		for(int[] q : queries) {
+			temp = arr[q[1]];
+			arr[q[1]] = arr[q[0]];
+			arr[q[0]] = temp;
+		}
+
+		System.out.println(Arrays.toString(arr));
+	}
+
+}
