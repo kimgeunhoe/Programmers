4ae76b255568cea31eba2748a68f72ad1f8f406c
kimgeunhoe <ghkim1031@naver.com>
2023-12-04 23:00:12 +0900

    Create RemoveVowels.java

---
+++ programmers_Lv0/RemoveVowels.java
@@ -0,0 +1,11 @@
+package programmers_Lv0;
+
+public class RemoveVowels {
+
+	public static void main(String[] args) {
+		String my_string = "nice to meet you";
+
+	    System.out.println(my_string.replaceAll("[aeiou]", ""));
+	}
+
+}
