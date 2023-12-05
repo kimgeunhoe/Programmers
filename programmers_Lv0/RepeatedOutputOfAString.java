238fc0856b0796d4f9c907df185f7c49e112b80a
kimgeunhoe <ghkim1031@naver.com>
2023-12-05 21:40:15 +0900

    Create RepeatedOutputOfAString.java

---
+++ programmers_Lv0/RepeatedOutputOfAString.java
@@ -0,0 +1,15 @@
+package programmers_Lv0;
+
+import java.util.Scanner;
+
+public class RepeatedOutputOfAString {
+//	https://school.programmers.co.kr/learn/courses/30/lessons/181950
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        int n = sc.nextInt();
+        
+        //System.out.print(str.repeat(n));
+	}
+
+}
