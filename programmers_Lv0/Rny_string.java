9a9d75d4eaa475eeb88310298edc204eab7cc9dc
kimgeunhoe <ghkim1031@naver.com>
2023-12-13 22:27:06 +0900

    Create Rny_string.java

---
+++ programmers_Lv0/Rny_string.java
@@ -0,0 +1,11 @@
+package programmers_Lv0;
+
+public class Rny_string {
+
+	public static void main(String[] args) {
+		String rny_string = "programmers";
+		
+		System.out.println(rny_string.replace("m", "rn"));
+	}
+
+}
