56f3423bf19850bf8f268490621bc08469049ab1
kimgeunhoe <ghkim1031@naver.com>
2023-12-18 22:28:42 +0900

    Create SeparateBySpaces1.java

---
+++ programmers_Lv0/SeparateBySpaces1.java
@@ -0,0 +1,13 @@
+package programmers_Lv0;
+
+import java.util.Arrays;
+
+public class SeparateBySpaces1 {
+
+	public static void main(String[] args) {
+		String my_string = "i love you";
+		
+		System.out.println(Arrays.toString(my_string.split(" ")));
+	}
+
+}
