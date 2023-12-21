f0676cdead3e11a0acf827feef3f4ff8fd774413
kimgeunhoe <ghkim1031@naver.com>
2023-12-21 21:40:09 +0900

    Update SharingPizza2.java

--- programmers_Lv0/SharingPizza2.java
+++ programmers_Lv0/SharingPizza2.java
@@ -1,20 +1,18 @@
 package programmers_Lv0;
 
 public class SharingPizza2 {
-
+//	https://school.programmers.co.kr/learn/courses/30/lessons/120815
 	public static void main(String[] args) {
 		int n = 4;
 	    int min = Math.min(n, 6);
-	    int num = 0;
 	    
-		for(int i=min; i>=1; i--) {
-			if(n%i==0 && 6%i==0) {
-				num = i;
+		for(; min>=1; min--) {
+			if(n%min==0 && 6%min==0) {
 				break;
 			}
 		}
 		
-	    System.out.println(n/num);
+	    System.out.println(n/min);
 	}
 
 }
