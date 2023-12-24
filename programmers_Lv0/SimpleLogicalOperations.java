31a32efc3337a3529c40ff337af7dd5222ffc3df
kimgeunhoe <ghkim1031@naver.com>
2023-12-24 21:02:38 +0900

    Create SimpleLogicalOperations.java

---
+++ programmers_Lv0/SimpleLogicalOperations.java
@@ -0,0 +1,14 @@
+package programmers_Lv0;
+
+public class SimpleLogicalOperations {
+//	https://school.programmers.co.kr/learn/courses/30/lessons/181917
+	public static void main(String[] args) {
+		boolean x1 = false;
+		boolean x2 = true;
+		boolean x3 = true;
+		boolean x4 = true;
+		
+		System.out.println(x1 | x2 && x3 | x4);
+	}
+
+}
