40373a75d806544cfb7ae411276eddeeea298dae
kimgeunhoe <ghkim1031@naver.com>
2024-01-02 21:59:20 +0900

    Create StringMultiplication.java

---
+++ programmers_Lv0/StringMultiplication.java
@@ -0,0 +1,12 @@
+package programmers_Lv0;
+
+public class StringMultiplication {
+//	https://school.programmers.co.kr/learn/courses/30/lessons/181940
+	public static void main(String[] args) {
+		String my_string = "string";
+		int k = 3;
+		
+		//System.out.println(my_string.repeat(k));
+	}
+
+}
