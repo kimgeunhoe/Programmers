16bab0f31dc17945d50b88556b5c21d11ddb58c2
kimgeunhoe <ghkim1031@naver.com>
2024-01-05 23:55:19 +0900

    Create Substring.java

---
+++ programmers_Lv0/Substring.java
@@ -0,0 +1,12 @@
+package programmers_Lv0;
+
+public class Substring {
+
+	public static void main(String[] args) {
+		String str1 = "abc";
+		String str2 = "aabcc";
+
+		System.out.println(str2.contains(str1) ? 1 : 0);
+	}
+
+}
