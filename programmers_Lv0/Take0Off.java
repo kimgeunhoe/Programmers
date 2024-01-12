a7109d8fe0d6ddd8c1fb1742ea8ced509c657f9c
kimgeunhoe <ghkim1031@naver.com>
2024-01-12 21:39:24 +0900

    Create Take0Off.java

---
+++ programmers_Lv0/Take0Off.java
@@ -0,0 +1,11 @@
+package programmers_Lv0;
+
+public class Take0Off {
+
+	public static void main(String[] args) {
+		String n_str = "0010";
+		
+		System.out.println(String.valueOf(Integer.parseInt(n_str)));
+	}
+
+}
