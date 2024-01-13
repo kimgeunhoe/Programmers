d297e1489d7d7a391007822faa46a2918af5ad05
kimgeunhoe <ghkim1031@naver.com>
2024-01-13 13:20:51 +0900

    Create TheFirstNCharacterOfAString.java

---
+++ programmers_Lv0/TheFirstNCharacterOfAString.java
@@ -0,0 +1,12 @@
+package programmers_Lv0;
+
+public class TheFirstNCharacterOfAString {
+//	https://school.programmers.co.kr/learn/courses/30/lessons/181907
+	public static void main(String[] args) {
+		String my_string = "ProgrammerS123";
+		int n = 11;
+		
+		System.out.println(my_string.substring(0, n));
+	}
+
+}
