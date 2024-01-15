d20114da401c1aa04762cd376e6437df73d3b98a
kimgeunhoe <ghkim1031@naver.com>
2024-01-15 19:48:17 +0900

    Create TheSumOfTheTwoNumbers.java

---
+++ programmers_Lv0/TheSumOfTheTwoNumbers.java
@@ -0,0 +1,14 @@
+package programmers_Lv0;
+
+import java.math.BigInteger;
+
+public class TheSumOfTheTwoNumbers {
+
+	public static void main(String[] args) {
+		String a = "582";
+		String b = "734";
+		
+		System.out.println(new BigInteger(a).add(new BigInteger(b)).toString());
+	}
+
+}
