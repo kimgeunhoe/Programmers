e940533bbff7cfb19d7024fdd5373ebaf49a3300
kimgeunhoe <ghkim1031@naver.com>
2024-01-25 21:54:22 +0900

    Create ToConvertAStringToAnInteger.java

---
+++ programmers_Lv0/ToConvertAStringToAnInteger.java
@@ -0,0 +1,11 @@
+package programmers_Lv0;
+
+public class ToConvertAStringToAnInteger {
+
+	public static void main(String[] args) {
+		String n_str = "10";
+		
+		System.out.println(Integer.parseInt(n_str));
+	}
+
+}
