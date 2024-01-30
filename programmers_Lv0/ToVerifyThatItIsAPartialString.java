6dc2064b46d5e07d8604b5a19cd5e220062b9159
kimgeunhoe <ghkim1031@naver.com>
2024-01-31 00:10:44 +0900

    Create ToVerifyThatItIsAPartialString.java

---
+++ programmers_Lv0/ToVerifyThatItIsAPartialString.java
@@ -0,0 +1,12 @@
+package programmers_Lv0;
+
+public class ToVerifyThatItIsAPartialString {
+
+	public static void main(String[] args) {
+		String my_string = "banana";
+		String target = "ana";
+		
+		System.out.println(my_string.contains(target) ? 1 : 0);
+	}
+
+}
