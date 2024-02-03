fcc8b47a18562c685e0c18805a2a525d8621faf9
kimgeunhoe <ghkim1031@naver.com>
2024-02-03 14:19:55 +0900

    Create ToVerifyThatItIsASubfix.java

---
+++ programmers_Lv0/ToVerifyThatItIsASubfix.java
@@ -0,0 +1,12 @@
+package programmers_Lv0;
+
+public class ToVerifyThatItIsASubfix {
+//	https://school.programmers.co.kr/learn/courses/30/lessons/181908
+	public static void main(String[] args) {
+		String my_string = "banana";
+		String is_suffix = "ana";
+		
+		System.out.println(my_string.endsWith(is_suffix) ? 1 : 0);
+	}
+
+}
