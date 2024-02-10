9d578548a55b95fac40605c4de6fa844e9ddf950
kimgeunhoe <ghkim1031@naver.com>
2024-02-10 17:16:39 +0900

    Update Budget.java

--- programmers_Lv1/Budget.java
+++ programmers_Lv1/Budget.java
@@ -7,24 +7,15 @@ public class Budget {
 	public static void main(String[] args) {
 		int[] d = {1, 3, 2, 5, 4};
 		int budget = 9;
-		int money = 0;
-		int answer = 0;
-		
-		Arrays.sort(d);
-		
-		for(int i=0; i<d.length; i++) {
-			money+= d[i];
-			
-			if(money>budget) {
-				answer = i;
-				break;
-			} else if(money==budget || i==d.length-1) {
-				answer = i + 1;
-				break;
-			}
-		}
-		
-		System.out.println(answer);
+        int answer = 0;
+        
+        Arrays.sort(d);
+        
+        for(; answer<d.length && budget>=d[answer]; answer++) {
+        	budget -= d[answer];
+        }
+        
+        System.out.println(answer);
 	}
 
 }
