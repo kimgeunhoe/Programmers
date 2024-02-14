d873b05b64a81adcf2c4674792aef29f07a7a9ac
kimgeunhoe <ghkim1031@naver.com>
2024-02-14 22:40:49 +0900

    Update CollatzConjecture.java

--- programmers_Lv1/CollatzConjecture.java
+++ programmers_Lv1/CollatzConjecture.java
@@ -3,15 +3,14 @@ package programmers_Lv1;
 public class CollatzConjecture {
 
 	public static void main(String[] args) {
-		long num = 626331;
-	    int answer = 0;
-
-	    while(num!=1) {
-	    	num = num%2==0 ? num/2 : num * 3 + 1;
-	    	answer++;
-	    }
-	    
-	    System.out.println(answer<500 ? answer : -1);
+		long num = 1;
+        int answer = 0;
+        
+        for(; answer<500 && num!=1; answer++) {
+        	num = num%2==0 ? num/2 : num*3+1;
+        }
+        
+        System.out.println(num==1 ? answer : -1);
 	}
 
 }
