1263bef86c93c56fccba598b2bb996ab4df06052
kimgeunhoe <ghkim1031@naver.com>
2024-02-23 23:07:00 +0900

    Update GymClothes.java

--- programmers_Lv1/GymClothes.java
+++ programmers_Lv1/GymClothes.java
@@ -9,31 +9,30 @@ public class GymClothes {
 //		int n = 5;
 //		int[] lost = {2, 4};
 //		int[] reserve = {1, 3, 5};
-		int answer = n;
 		int[] student = new int[n];
 		
-		for(int l : lost) {
-			student[l-1]--;
-		}
-		
-		for(int r : reserve) {
-			student[r-1]++;
-		}
-		
-		for(int i=0; i<n; i++) {
-			if(student[i]==-1) {
-				if(i>0 && student[i-1]==1) {
-					student[i-1]--;
-					student[i]++;
-				} else if(i<n-1 && student[i+1]==1) {
-					student[i+1]--;
-					student[i]++;
-				} else {
-					answer--;
-				}
-			}
-		}
-		
-		System.out.println(answer);
+        for(int l : lost) {
+        	student[l-1]--;
+        }
+    
+        for(int r : reserve) {
+        	student[r-1]++;
+        }
+        
+        for(int i=0; i<student.length; i++) {
+        	if(student[i]==-1) {
+        		if(i>0 && student[i-1]==1) {
+        			student[i-1] = 0;
+        			student[i] = 0;
+        		} else if(i<student.length-1 && student[i+1]==1){
+        			student[i+1] = 0;
+        			student[i] = 0;
+        		} else {
+        			n--;
+        		}
+        	}
+        }
+        
+        System.out.println(n);
 	}
 }
