d251176e9acf9b6e20a20c1340c5f96bd893fe1a
kimgeunhoe <ghkim1031@naver.com>
2024-02-25 14:54:18 +0900

    Update HarshadNumber.java

--- programmers_Lv1/HarshadNumber.java
+++ programmers_Lv1/HarshadNumber.java
@@ -5,12 +5,11 @@ public class HarshadNumber {
 	public static void main(String[] args) {
 		int x = 10;
 		int num = x;
-		int sum = 0;
-		
-		do {
-			sum += num%10;
-			num = (int) Math.ceil(num/10);
-		} while(num>=1);
+        int sum = 0;
+        
+        for(; num>0; num/=10) {
+            sum += num%10;
+        }
 		
 		System.out.println(x%sum==0);
 	}
