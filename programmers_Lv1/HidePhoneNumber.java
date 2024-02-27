cad831da91480e05d73de1c8f7cc238e19941cf9
kimgeunhoe <ghkim1031@naver.com>
2024-02-27 23:29:56 +0900

    Update HidePhoneNumber.java

--- programmers_Lv1/HidePhoneNumber.java
+++ programmers_Lv1/HidePhoneNumber.java
@@ -4,12 +4,18 @@ public class HidePhoneNumber {
 
 	public static void main(String[] args) {
 		String phone_number = "01033334444";
-		String answer = "";
+		StringBuilder sb = new StringBuilder();
+	    
+	    for(int i=0; i<phone_number.length()-4; i++) {
+	    	sb.append('*');
+	    }
+	    
+	    sb.append(phone_number.substring(phone_number.length()-4));
+	    
+	    System.out.println(sb.toString());
 		
-		answer = phone_number.substring(0, phone_number.length()-4).replaceAll("[0-9]", "*") + phone_number.substring(phone_number.length()-4, phone_number.length());
-//		phone_number.replaceAll(".(?=.{4})", "*");
-		
-		System.out.println(answer);
+	    System.out.println(phone_number.replaceAll(".(?=.{4})", "*"));
+	    //https://shrimp-burger.tistory.com/149
 	}
 
 }
