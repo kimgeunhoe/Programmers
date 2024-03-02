928899258ef8e23b19b94ba4c466ee541f1f862f
kimgeunhoe <ghkim1031@naver.com>
2024-03-02 15:13:42 +0900

    Update LottoRank.java

--- programmers_Lv1/LottoRank.java
+++ programmers_Lv1/LottoRank.java
@@ -1,38 +1,37 @@
 package programmers_Lv1;
 
+import java.util.Arrays;
+
 public class LottoRank {
 
 	public static void main(String[] args) {
 		int[] lottos = {44, 1, 0, 0, 31, 25};
 		int[] win_nums = {31, 10, 45, 1, 6, 19};
-		int o = 0;
-		int sameNum = 0;
+		int[] answer = {0, 7};
+        int zero = 0;
+        
+        for(int l : lottos) {
+        	if(l==0) {
+        		zero++;
+        	} else {
+        		for(int w : win_nums) {
+        			if(l==w) {
+        				answer[1]--;
+        				break;
+        			}
+        		}
+        	}
+        }
+        
+        answer[0] = answer[1] - zero;
+        
+        for(int i=0; i<answer.length; i++) {
+        	if(answer[i]==7) {
+        		answer[i]--;
+        	}
+        }
 		
-		for(int l : lottos) {
-			if(l==0) {
-				o++;
-			} else {
-				for(int w : win_nums) {
-					if(l==w) {
-						sameNum++;
-						break;
-					}
-				}
-			}
-		}
-		
-		int high = 7 - o - sameNum;
-		int low = 7 - sameNum;
-		
-		if(high==7) {
-			high = 6;
-		}
-
-		if(low==7) {
-			low = 6;
-		}
-		
-		System.out.print(high + ", " + low);
+		System.out.print(Arrays.toString(answer));
 	}
 
 }
