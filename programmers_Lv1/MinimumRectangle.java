b8e9affa1c326b0fcad88090ba720a06a4f98570
kimgeunhoe <ghkim1031@naver.com>
2024-03-05 21:34:09 +0900

    Update MinimumRectangle.java

--- programmers_Lv1/MinimumRectangle.java
+++ programmers_Lv1/MinimumRectangle.java
@@ -5,15 +5,15 @@ public class MinimumRectangle {
 	public static void main(String[] args) {
 //		int[][] sizes = {{60, 50}, {30, 70}, {60, 30}, {80, 40}};
 		int[][] sizes = {{14, 4}, {19, 6}, {6, 16}, {18, 7}, {7, 11}};
-		int l1 = 0;
-		int l2 = 0;
-		
-		for(int[] size : sizes) {
-			l1 = Math.max(l1, Math.max(size[0], size[1]));
-			l2 = Math.max(l2, Math.min(size[0], size[1]));
-		}
-		
-		System.out.println(l1*l2);
+	    int x = 0;
+	    int y = 0;
+	    
+	    for(int[] s : sizes) {
+	    	x = Math.max(x, Math.max(s[0], s[1]));
+	    	y = Math.max(y, Math.min(s[0], s[1]));
+	    }
+	    
+	    System.out.println(x * y);
 	}
 
 }
