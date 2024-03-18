9fd2dcb0f913d91279a951993166d3eee8afc08e
kimgeunhoe <ghkim1031@naver.com>
2024-03-18 22:02:00 +0900

    Update Retire.java

--- programmers_Lv1/Retire.java
+++ programmers_Lv1/Retire.java
@@ -9,21 +9,22 @@ public class Retire {
 		String[] participant = {"mislav", "stanko", "mislav", "ana"};
 		String[] completion = {"stanko", "ana", "mislav"};
 		String answer = "";
-		Map<String, Integer> map = new HashMap<>();
-		
-		for(String p : participant) {
-			map.put(p, map.getOrDefault(p, 0)+1);
-		}
-		
-		for(String c : completion) {
-			map.replace(c, map.get(c)-1);
-		}
-		
-		for(String p : participant) {
-			if(map.get(p)==1) {
-				answer = p;
-			}
-		}
+        Map<String, Integer> map = new HashMap<>();
+        
+        for(String p : participant) {
+        	map.put(p, map.getOrDefault(p, 0) + 1);
+        }
+        
+        for(String c : completion) {
+        	map.put(c, map.get(c)-1);
+        }
+        
+        for(String k : map.keySet()) {
+        	if(map.get(k)!=0) {
+        		answer = k;
+        		break;
+        	}
+        }
 		
 		System.out.println(answer);
 	}
