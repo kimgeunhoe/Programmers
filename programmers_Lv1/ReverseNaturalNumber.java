05ad073297ef5357662927110ffd872744db5f19
kimgeunhoe <ghkim1031@naver.com>
2024-03-19 22:35:40 +0900

    Update ReverseNaturalNumber.java

--- programmers_Lv1/ReverseNaturalNumber.java
+++ programmers_Lv1/ReverseNaturalNumber.java
@@ -1,20 +1,19 @@
 package programmers_Lv1;
 
+import java.util.Arrays;
+
 public class ReverseNaturalNumber {
 
 	public static void main(String[] args) {
 		long n = 12345;
-		String num = String.valueOf(n);
-		int[] answer = new int[num.length()];
-		int index = num.length() - 1;
-		
-		for(int i=0; i<num.length(); i++) {
-			answer[index--] = Character.getNumericValue(num.charAt(i));
-		}
-		
-		for(int a : answer) {
-	        System.out.println(a);
-	    }
+		int length = String.valueOf(n).length();
+        int[] answer = new int[length];
+ 
+        for(int i=0; i<length; i++, n/=10) {
+            answer[i] = (int)(n%10);
+        }
+        
+        System.out.println(Arrays.toString(answer));
 	}
 
 }
