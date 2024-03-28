2f989ad2e4e0b3663a936e50268b4c978993c4c0
kimgeunhoe <ghkim1031@naver.com>
2024-03-28 21:09:50 +0900

    Update SumCipher.java

--- programmers_Lv1/SumCipher.java
+++ programmers_Lv1/SumCipher.java
@@ -4,12 +4,11 @@ public class SumCipher {
 
 	public static void main(String[] args) {
 		int n = 987;
-		int m = n;
 	    int answer = 0;
 
-	    while(m>=1) {
-	    	answer += m%10;
-	    	m /= 10;
+	    while(n>=1) {
+	    	answer += n%10;
+	    	n /= 10;
 	    }
 	    
 	    System.out.println(answer);
