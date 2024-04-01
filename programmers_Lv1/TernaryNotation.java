75683b5a1f734f03093c8cced27a6443a1263030
kimgeunhoe <ghkim1031@naver.com>
2024-04-01 22:28:14 +0900

    Update TernaryNotation.java

--- programmers_Lv1/TernaryNotation.java
+++ programmers_Lv1/TernaryNotation.java
@@ -5,10 +5,10 @@ public class TernaryNotation {
 	public static void main(String[] args) {
 		int n = 45;
 		int answer = 0;
-		String[] tn = Integer.toString(n, 3).split("");
+		String tn = Integer.toString(n, 3);
 
-		for(int i=0; i<tn.length; i++) {
-			answer += Integer.parseInt(tn[i])*Math.pow(3, i);
+		for(int i=0; i<tn.length(); i++) {
+			answer += Character.getNumericValue(tn.charAt(i))*Math.pow(3, i);
 		}
 		
 		System.out.println(answer);
