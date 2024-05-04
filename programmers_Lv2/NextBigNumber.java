31663de02ede1624b0e09f9a65e075d2a6d377b9
kimgeunhoe <ghkim1031@naver.com>
2024-05-04 17:05:20 +0900

    Create NextBigNumber.java

---
+++ programmers_Lv2/NextBigNumber.java
@@ -0,0 +1,60 @@
+package programmers_Lv2;
+
+public class NextBigNumber {
+
+	public static void main(String[] args) {
+		int n = 78;
+//		int n = 8;
+//	    String binary = Integer.toBinaryString(n);
+//	    String s = "";
+//	    int index = 0;
+//	    int count = -1;
+//	    StringBuilder sb = new StringBuilder();
+//	    
+//	    index = binary.lastIndexOf("01");
+//	    
+//	    if(index!=-1) {
+//	    	sb.append(binary.substring(0, index)).append('1');
+//	    	s = binary.substring(index+1);
+//	    } else {
+//	    	sb.append("10");
+//	    	s = binary.substring(1);
+//	    	count++;
+//	    }
+//	    
+//	    for(int i=0; i<s.length(); i++) {
+//	    	if(s.charAt(i)=='1') {
+//	    		count++;
+//	    	}
+//	    }
+//	    
+//	    for(int i=0; i<s.length(); i++) {
+//	    	if(s.length()-i!=count) {
+//	    		sb.append('0');
+//	    	} else {
+//	    		sb.append('1');
+//	    		count--;
+//	    	}
+//	    }
+//	    
+//	    System.out.println(Integer.parseInt(sb.toString(), 2));
+	    
+		int count = Integer.bitCount(n);
+	    int compare = n+1;
+
+	    while(true) {
+	      if(Integer.bitCount(compare)==count) {
+	    	  break;
+	      }
+
+	      compare++;
+	    }
+
+	    System.out.println(compare);
+	    
+//	    int postPattern = n & -n;
+//	    int smallPattern = ((n ^ (n + postPattern)) / postPattern) >> 2;
+//      System.out.println(n + postPattern | smallPattern);
+	}
+
+}
