8eaea0404c2b7b366288e1c8ec069b393e14d3fa
kimgeunhoe <ghkim1031@naver.com>
2024-06-24 16:12:49 +0900

    Update CorrectParentheses.java

--- programmers_Lv2/CorrectParentheses.java
+++ programmers_Lv2/CorrectParentheses.java
@@ -3,10 +3,10 @@ package programmers_Lv2;
 //import java.util.Stack;
 
 public class CorrectParentheses {
-
+//	https://school.programmers.co.kr/learn/courses/30/lessons/12909
 	public static void main(String[] args) {
 		String s = "()()";
-	    boolean answer = true;
+//	    boolean answer = true;
 //	    Stack<Character> stack = new Stack<>();
 		int check = 0;
 //
@@ -26,24 +26,11 @@ public class CorrectParentheses {
 //	    System.out.println(answer);
 //	    System.out.println(stack.empty());
 		
-		for(int i=0; i<s.length(); i++) {
-			if(s.charAt(i)=='(') {
-	    		check++;
-	    	} else {
-	    		if(check==0) {
-	    			answer = false;
-	    			break;
-	    		} else {
-	    			check--;
-	    		}
-	    	}
+		for(int i=0; i<s.length() && check>=0; i++) {
+			check += s.charAt(i)=='(' ? 1 : -1;
 		}
 		
-		if(check!=0) {
-			System.out.println(false);
-		} else {
-			System.out.println(answer);
-		}
+		System.out.println(check==0);
 	}
 
 }
