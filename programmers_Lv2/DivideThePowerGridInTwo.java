02c3bbe39a9b55169b044988f63ef440a30ca177
kimgeunhoe <ghkim1031@naver.com>
2024-07-25 22:00:53 +0900

    Create DivideThePowerGridInTwo.java

---
+++ programmers_Lv2/DivideThePowerGridInTwo.java
@@ -0,0 +1,13 @@
+package programmers_Lv2;
+
+public class DivideThePowerGridInTwo {
+//	https://school.programmers.co.kr/learn/courses/30/lessons/86971
+	public static void main(String[] args) {
+		int n = 9;
+		int[][] wires = {{1, 3}, {2, 3}, {3, 4}, {4, 5}, {4, 6}, {4, 7}, {7, 8}, {7, 9}};
+		int answer = -1;
+
+		
+	}
+
+}
